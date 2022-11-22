504ff18052ae3259762097a4b71544f4bc8dc45a
Ethan Willetts <willetts.ethan@gmail.com>
2022-11-22 09:13:47 -0500

    commit tree a1e38774f8c487bdbac0053ed1cab32e0b95483e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    return arrayOfDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(arrayOfDrivers) {
+    return arrayOfDrivers.slice(2, 4);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(numberOfDrivers) {
+    return function fareCost(fare) {
+        return numberOfDrivers * fare;
+    };
+};
+
+const fareDoubler = function(fare) {
+    return function doubler(fare) {
+        return fare * 2;
+    };
+}
